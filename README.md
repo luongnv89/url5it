8b6d93bd1dc6915c6fee915837ec6f2fd87dbecf
Nguyen Van Luong <luongnv89@gmail.com>
2014-07-09 15:32:58 +0200

    Initial web page

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ tic-url
 =======
 
 Convert a long-url to very short-url for using on twitter
+
+-------------------------------
+Nguyen Van Luong - luongnv89@gmail.com - [http://luongnv.info](http://luongnv.info)
---
+++ js/script.js
@@ -0,0 +1,53 @@
+function startingPoint () {
+  var shortURL = window.location.search;
+  console.log('shortURL: ' + shortURL);
+  if(shortURL.length!=""){
+    redirectURL = getRealURL(shortURL);
+    console.log('redirectURL: ' + redirectURL);
+    if(validateUrl(redirectURL)){
+      window.location.replace(redirectURL);
+    }else{
+      console.log('The redirect url is not validate');
+    }
+  }
+  var btConvert = document.getElementById('btConvert');
+  btConvert.addEventListener('click',convertURL,false);
+}
+
+convertURL = function () {
+  var shortURL='';
+  var inputURL = document.getElementById('input-url');
+  if(validateUrl(inputURL.value)){
+    shortURL = window.location+"?"+hashCode(inputURL.value);
+    sendToServer(shortURL,inputURL);
+    document.getElementById('short-url').innerHTML = shortURL;
+  }else{
+    document.getElementById('short-url').innerHTML = "Your input-url is not validate";
+  }
+}
+
+function validateUrl(value){
+      return /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(value);
+}
+
+hashCode = function(str){
+    var hash = 0;
+    if (str.length == 0) return hash;
+    for (i = 0; i < str.length; i++) {
+        char = str.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
+
+sendToServer = function (shortURL,inputURL) {
+  console.log('Save url to server');
+}
+
+getRealURL = function (shortURL) {
+  console.log('Query to get URL from server');
+  return window.location.origin + window.location.pathname;
+}
+
+document.addEventListener('DOMContentLoaded',startingPoint,false);
