95e4a7009d1ab479a1f8fe08e76e77d5c5ee2909
Nguyen Van Luong <luongnv89@gmail.com>
2014-07-10 16:57:36 +0200

    Update

--- php/connectdb.php
+++ php/connectdb.php
@@ -2,6 +2,7 @@
 $conn = mysql_connect('localhost','root','280189');
 if(!$conn){
 	die('Could not connect: '.mysql_error());
+	echo 'Could not connect: '.mysql_error();
 }
 //echo 'Connected sucessfull';
 mysql_select_db('ticdb') or die('Could not select database');
